f628c37893d222c12cd1d85d30a1f634d7b750dc
Jaaunius <r2rrr50@gmail.com>
2019-01-23 15:31:58 +0200

    2019-01-23

--- test.py
+++ test.py
@@ -17,12 +17,15 @@ enemyX = [0] * 5
 enemyY = [0] * 5
 
 index = 0
+index1 = 1
 
 snakeX = 500
 snakeY = 500
 
 enemyX[0] = randint(0, 800)
 enemyY[0] = 50
+enemyX[1] = randint(0, 800)
+enemyY[1] = 50
 
 win = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Falling Game')
@@ -30,11 +33,19 @@ pygame.display.set_caption('Falling Game')
 def player(block_size):
     pygame.draw.rect(win, white, [snakeX, snakeY, block_size, block_size])
 
-def enemy(block_size,x,y):
-    pygame.draw.ellipse(win, red, [x, y, block_size, block_size])
+def enemy(x,y):
+    pygame.draw.ellipse(win, red, [enemyX[0], enemyY[0], 30, 30])
+    pygame.draw.ellipse(win, red, [enemyX[1], enemyY[1], 30, 30])
 
 def enemyLower():
     enemyY[0] += 5
+    enemyY[1] += 5
+
+def clone():
+    enemyY[0] = 0
+    enemyX[0] = randint(0, 800)
+    enemyY[1] = 0
+    enemyX[1] = randint(0, 800)
 
 run = True
 while run:
@@ -51,10 +62,16 @@ while run:
     if keys[pygame.K_RIGHT]:
         snakeX += vel
 
+    if enemyY[0] >= display_height:
+        clone()
+
+    if enemyY[1] >= display_height:
+        clone()
+
     win.fill(black)
 
     player(40)
-    enemy(30,500,300)
+    enemy(500,300)
     enemyLower()
 
     pygame.display.update()
