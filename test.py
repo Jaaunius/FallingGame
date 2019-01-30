12a129bfc9229f161e2685106788a70ed4017485
Jaaunius <r2rrr50@gmail.com>
2019-01-30 15:32:11 +0200

    2019-01-30

--- test.py
+++ test.py
@@ -13,19 +13,14 @@ display_height = 600
 
 vel = 15
 
-enemyX = [0] * 5
-enemyY = [0] * 5
-
 index = 0
 index1 = 1
 
 snakeX = 500
 snakeY = 500
 
-enemyX[0] = randint(0, 800)
-enemyY[0] = 50
-enemyX[1] = randint(0, 800)
-enemyY[1] = 50
+enemyX = [randint(0, 800), randint(0, 800), randint(0, 800), randint(0, 800), randint(0, 800)]
+enemyY = [50, 50, 50, 50, 50]
 
 win = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Falling Game')
@@ -36,16 +31,28 @@ def player(block_size):
 def enemy(x,y):
     pygame.draw.ellipse(win, red, [enemyX[0], enemyY[0], 30, 30])
     pygame.draw.ellipse(win, red, [enemyX[1], enemyY[1], 30, 30])
+    pygame.draw.ellipse(win, red, [enemyX[2], enemyY[2], 30, 30])
+    pygame.draw.ellipse(win, red, [enemyX[3], enemyY[3], 30, 30])
+    pygame.draw.ellipse(win, red, [enemyX[4], enemyY[4], 30, 30])
 
 def enemyLower():
     enemyY[0] += 5
     enemyY[1] += 5
+    enemyY[2] += 5
+    enemyY[3] += 5
+    enemyY[4] += 5
 
 def clone():
     enemyY[0] = 0
     enemyX[0] = randint(0, 800)
     enemyY[1] = 0
     enemyX[1] = randint(0, 800)
+    enemyY[2] = 0
+    enemyX[2] = randint(0, 800)
+    enemyY[3] = 0
+    enemyX[3] = randint(0, 800)
+    enemyY[4] = 0
+    enemyX[4] = randint(0, 800)
 
 run = True
 while run:
@@ -67,6 +74,21 @@ while run:
 
     if enemyY[1] >= display_height:
         clone()
+
+    if snakeX == enemyX[0] and snakeY == enemyY[0]:
+        break
+
+    if snakeX == enemyX[1] and snakeY == enemyY[1]:
+        break
+
+    if snakeX == enemyX[2] and snakeY == enemyY[2]:
+        break
+
+    if snakeX == enemyX[3] and snakeY == enemyY[3]:
+        break
+
+    if snakeX == enemyX[4] and snakeY == enemyY[4]:
+        break
 
     win.fill(black)
 
